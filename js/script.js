76b58f0362b183cab76e1a8fc64d9720d1bba68c
frgwork <frgwork@ya.ru>
2022-01-14 02:13:05 +0300

    Consent Popup Box

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.querySelector('.open-btn').onclick = () => {
+   document.querySelector('.cookies-box').classList.add('active');
+}
+document.querySelector('.close-btn').onclick = () => {
+   document.querySelector('.cookies-box').classList.remove('active');
+}
